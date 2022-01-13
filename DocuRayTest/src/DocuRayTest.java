0d966fcdab8d72cf0612b26b55c7de5055bd43fa
BluemoonSoft <BluemoonSoft@DESKTOP-9MT1MAF>
2022-01-13 14:56:06 +0900

    asdad

--- DocuRayTest/src/DocuRayTest.java
+++ DocuRayTest/src/DocuRayTest.java
@@ -24,3 +24,1378 @@ public class DocuRayTest {
 17
 18
 20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
+public class DocuRayTest {
+	public static void main(String[] args) {
+		System.out.println("DocuRay Test!!!!!");
+		// 12345
+	}
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+20
